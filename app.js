20b7505602cd8f33568a8e31fa5110a16ff3b64f
SamohvalovGMcR <riniant77@gmail.com>
2017-06-22 21:36:49 +0500

    server

---
+++ app.js
@@ -0,0 +1,90 @@
+﻿var http = require('http');
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var config = require('./config');
+
+var app = express();
+
+app.set('port', config.get('port'));
+app.set('ip', config.get('ip'));
+ 
+var server = http.createServer(app).listen(app.get('port'),config.get('ip'), function(){
+  console.log('Express server listening on port=' + config.get('port')+",  ip="+config.get('ip'));
+});
+
+
+
+var io = require('socket.io')(server);
+
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+  res.io = io;
+  next();
+});
+
+
+app.use('/', index);
+app.use('/users', users);
+app.get('/slider',function(req, res, next){
+res.sendfile("slider.html")
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var value={} ;
+
+
+
+io.sockets.on('connection', function (client) {
+
+
+client.emit('new', {});
+
+    
+  
+});//'connection', function 
+ 
+
+//module.exports = app;
+
+module.exports = {app: app, server: server};
+
